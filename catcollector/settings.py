0fe0928a83a5212f24fdfcd6cd0aac28382de5ae
Denis S Dujota <dujota@Deniss-MacBook-Pro.local>
2022-10-25 19:21:56 -0500

    update location of heroku locals

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/cats/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+django_heroku.settings(locals())
